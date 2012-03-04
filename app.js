545ee9e303d5a0bf40cd30c4738e42c3780273d4
Josh Vermaire <joshvermaire@gmail.com>
2012-03-04 02:19:33 -0800

    test with express

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+var express = require('express');
 var Bot    = require('ttapi');
 var AUTH   = 'auth+live+1a85d504c00feb6e6c815f3e4d9a6319ef65354a';
 var USERID = '4f533573a3f751580f004897';
 var ROOMID = '4f533e97a3f751580f0048f4'; //Brilliant '4f4db9770c4cc87adc0cad76';
-
+var app = express.createServer();
 var bot = new Bot(AUTH, USERID);
 
 bot.debug = true;
@@ -13,3 +14,9 @@ bot.on('roomChanged',  function (data) { console.log('The bot has changed room.'
 bot.on('speak',        function (data) { console.log('Someone has spoken', data); });
 bot.on('update_votes', function (data) { console.log('Someone has voted',  data); });
 bot.on('registered',   function (data) { console.log('Someone registered', data); });
+
+
+var port = process.env.PORT || 8924;
+app.listen(port, function(){
+  console.log("Listening on " + port);
+});
